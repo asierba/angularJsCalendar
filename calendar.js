b6204eadf00d971b35ae83b3fe4ea63e6aaa5e90
Asier Barrenetxea <asier.barrenetxea-arando@wiggle.co.uk>
2013-10-25 18:46:23 +0100

    some more changes

--- calendar.js
+++ calendar.js
@@ -3,6 +3,41 @@ function CalendarCtrl($scope) {
 	var currentMonth = d.getMonth();	
 	$scope.year = d.getFullYear();
 
+	$scope.days = [
+        null,       // 0
+        "Asier",    // 1
+        "Asier",    // 2
+        "Asier",    // 3
+         null,      // 4
+         null,      // 5
+         null,      // 6
+         null,      // 7
+         null,      // 8
+         null,      // 9
+         null,      // 10
+         null,      // 11
+         null,      // 12
+         null,      // 13
+         null,      // 14
+         "Herny",   // 15
+         "Herny",   // 16
+         "Herny",   // 17
+         "Herny",   // 18
+         "Herny",   // 19
+         "Herny",   // 20
+         null,      // 21
+         null,      // 22
+         null,      // 23
+         null,      // 24
+         null,      // 25
+         "Jorge",   // 26
+         "Jorge",   // 27
+         "Jorge",   // 28
+         "Jorge",   // 29
+         "Jorge",   // 30
+         "Jorge",   // 31
+	];
+
 	$scope.bookings = [
 		{who: "Asier", from: new Date(2013,9,12) , to: new Date(2013,9,25)},
 		{who: "Jorge", from: new Date(2013,9,01) , to: new Date(2013,9,02)},
