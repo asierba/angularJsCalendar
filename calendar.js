4649b498aaae6a1f9eb39847ec3d1bf9b3d0984b
Asier Barrenetxea <asierba@gmail.com>
2013-10-31 23:17:25 +0000

    events removed, just focus in the calendar

--- calendar.js
+++ calendar.js
@@ -3,69 +3,49 @@ function CalendarCtrl($scope) {
 	var currentMonth = d.getMonth();	
 	$scope.year = d.getFullYear();
 
-	$scope.days = [
-        null,       // 0
-        "Asier",    // 1
-        "Asier",    // 2
-        "Asier",    // 3
-         null,      // 4
-         null,      // 5
-         null,      // 6
-         null,      // 7
-         null,      // 8
-         null,      // 9
-         null,      // 10
-         null,      // 11
-         null,      // 12
-         null,      // 13
-         null,      // 14
-         "Herny",   // 15
-         "Herny",   // 16
-         "Herny",   // 17
-         "Herny",   // 18
-         "Herny",   // 19
-         "Herny",   // 20
-         null,      // 21
-         null,      // 22
-         null,      // 23
-         null,      // 24
-         null,      // 25
-         "Jorge",   // 26
-         "Jorge",   // 27
-         "Jorge",   // 28
-         "Jorge",   // 29
-         "Jorge",   // 30
-         "Jorge",   // 31
-	];
+        var monthNames = ["January", "February", "March", "April", "May", "June", 
+                "July", "August", "September", "October", "November", "December"];      
 
-	$scope.bookings = [
-		{who: "Asier", from: new Date(2013,9,12) , to: new Date(2013,9,25)},
-		{who: "Jorge", from: new Date(2013,9,01) , to: new Date(2013,9,02)},
-		{who: "Henry", from: new Date(2013,9,26) , to: new Date(2013,9,29)}
-	];
+        var calculateDaysForMonth = function (month) {
+                if(monthNames[month] === "February") {
+                      return new Array(28);  
+                }
+                if(monthNames[month] === "April") {
+                      return new Array(30);  
+                }
 
-	var monthNames = ["January", "February", "March", "April", "May", "June", 
-					"July", "August", "September", "October", "November", "December"];	
+                return new Array(31);
+        };
+
+        $scope.days = calculateDaysForMonth(currentMonth);	
 
 	$scope.month = function () {
 		return monthNames[currentMonth];
 	};
 
 	$scope.previousMonth = function () {
+                $scope.days = new Array(11);
+
 		if (currentMonth == 0) {
 			currentMonth = 11;
 			$scope.year--
 		} else {
 			currentMonth--;
 		}
+
+                $scope.days = calculateDaysForMonth(currentMonth);
 	};
 
 	$scope.nextMonth = function () {
+                $scope.days = new Array(66);
+
 		if (currentMonth == 11) {
 			currentMonth = 0;
 			$scope.year++
 		} else {
 			currentMonth++;
-		}		
+		}
+
+                $scope.days = calculateDaysForMonth(currentMonth);		
 	};
 }
