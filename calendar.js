abff21357ab32d58cc4f2ab42e26bdb8f865fd09
Asier Barrenetxea <1537266+asierba@users.noreply.github.com>
2013-11-01 17:23:40 +0000

    Calendar working 	- Jasmine tests added 	- Jasmine and Angular js added to library folder

--- calendar.js
+++ calendar.js
@@ -1,31 +1,85 @@
 function CalendarCtrl($scope) {
-	var d = new Date();
-	var currentMonth = d.getMonth();	
-	$scope.year = d.getFullYear();
+    var currentMonth = 0,
+    	monthNames = ["January", "February", "March", "April", "May", "June", 
+            "July", "August", "September", "October", "November", "December"];
 
-        var monthNames = ["January", "February", "March", "April", "May", "June", 
-                "July", "August", "September", "October", "November", "December"];      
+	var numbeOfDaysInMonth = function(month) {
+		switch(monthNames[month])
+    	{
+    		case "February":
+    			return 28;
+    		break;
 
-        var calculateDaysForMonth = function (month) {
-                if(monthNames[month] === "February") {
-                      return new Array(28);  
-                }
-                if(monthNames[month] === "April") {
-                      return new Array(30);  
-                }
-
-                return new Array(31);
-        };
-
-        $scope.days = calculateDaysForMonth(currentMonth);	
-
-	$scope.month = function () {
-		return monthNames[currentMonth];
+    		case "April":
+    		case "June":
+    		case "September":
+    		case "November":
+    			return 30;
+    		break;
+    		
+    		case "January":
+    		case "March":
+    		case "May":
+    		case "July":
+    		case "August":
+    		case "October":
+    		case "December":
+    			return 31;
+    	}
 	};
 
-	$scope.previousMonth = function () {
-                $scope.days = new Array(11);
+	var createDaysNonThisMonth = function(min, max) {
+		var days = [];
+		for(var i=min; i<= max; i++) {
+			days.push({value: i, isNotThisMonth: true});
+		}
+		return days;
+	};
 
+    var createDaysPreviousMonth = function() {
+    	var firstDayOfMonth = new Date($scope.year, currentMonth, 1, 0, 0, 0, 0);
+		var firstDayOfMonthIndex = firstDayOfMonth.getDay() === 0 ? 7 : firstDayOfMonth.getDay();
+
+
+		var previousMonth = currentMonth === 0 ? 11 : currentMonth-1;
+
+		var numOfDaysPreviousMonth = numbeOfDaysInMonth(previousMonth);
+		var mondayPreviousMonth = numOfDaysPreviousMonth-firstDayOfMonthIndex+2;
+
+    	return createDaysNonThisMonth(mondayPreviousMonth, numOfDaysPreviousMonth);
+    };
+
+    var createDaysNextMonth = function() {    	
+    	var lastDayOfMonth = new Date($scope.year, currentMonth, numbeOfDaysInMonth(currentMonth), 0, 0, 0, 0);
+		var lastDayOfMonthIndex = lastDayOfMonth.getDay() === 0 ? 7 : lastDayOfMonth.getDay();
+		var daysleftOnWeek = 7 - lastDayOfMonthIndex;
+		
+		return createDaysNonThisMonth(1, daysleftOnWeek);
+    };
+
+    var createDaysThisMonth = function(num) {    	
+		var days = [];
+		for(var i=1; i<= num; i++) {
+			days.push({value: i});
+		}
+		return days;
+	};
+
+    var calculateDaysForMonth = function(month) {
+    	return  createDaysThisMonth(numbeOfDaysInMonth(month));
+    };  
+
+    var drawMonth = function () {    	
+		$scope.days = createDaysPreviousMonth()
+			.concat(calculateDaysForMonth(currentMonth))
+			.concat(createDaysNextMonth());	
+    };   
+  
+	$scope.month = function() {
+		return monthNames[currentMonth];
+	};	
+
+	$scope.previousMonth = function () {      
 		if (currentMonth == 0) {
 			currentMonth = 11;
 			$scope.year--
@@ -33,12 +87,10 @@ function CalendarCtrl($scope) {
 			currentMonth--;
 		}
 
-                $scope.days = calculateDaysForMonth(currentMonth);
+        drawMonth();
 	};
 
-	$scope.nextMonth = function () {
-                $scope.days = new Array(66);
-
+	$scope.nextMonth = function() {
 		if (currentMonth == 11) {
 			currentMonth = 0;
 			$scope.year++
@@ -46,6 +98,16 @@ function CalendarCtrl($scope) {
 			currentMonth++;
 		}
 
-                $scope.days = calculateDaysForMonth(currentMonth);		
-	};
+       	drawMonth();	
+	};	
+
+ 	var initialise = function() {
+		var d = new Date();
+		currentMonth = d.getMonth();
+		$scope.year = d.getFullYear();		
+
+		drawMonth();		
+	};	
+
+	initialise();
 }
